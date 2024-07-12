46976ad95979e377071a4fd208a872b6fc49e616
Santje777 <s.v.oosterom@gmail.com>
2024-07-12 16:12:02 +0200

    Added a dictionary with capital cities as a value

--- main.py
+++ main.py
@@ -14,3 +14,28 @@ countries= ["France", "Germany", "Belgium"]
 
 for country in countries:
     print(f"My number {countries.index(country)+1} choice is {country}")
+
+"""a dictionary of 3 countries I’d like to visit as a key and their capital city as a value"""
+
+countries= {"France":"Paris", "Germany":"Berlin", "Belgium":"Brussels"}
+print(countries)
+del countries["Belgium"]
+print(countries)
+countries["Spain"] = "Madrid"
+print(countries)
+print(countries["France"])
+print(countries["Germany"])
+print(countries["Spain"])
+
+#looping through each key
+for country in countries:
+    capital = countries[country]
+    print(f"The capital of {country} is {countries[country]}")
+
+print("Countries I'd like to visit:")
+
+#looping through each key-value pair
+index = 0
+for country, capital in countries.items():
+    print(f"{index+1}. {country} (Capital city:{capital})")
+    index = index + 1
