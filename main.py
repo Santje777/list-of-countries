f03e40c162ec419bc52310a15a394527bf2facde
Santje777 <s.v.oosterom@gmail.com>
2024-07-13 14:54:40 +0200

    Added list of dictionaries with calculation of total population in millions

--- main.py
+++ main.py
@@ -39,3 +39,33 @@ index = 0
 for country, capital in countries.items():
     print(f"{index+1}. {country} (Capital city:{capital})")
     index = index + 1
+
+"""Calculation of total of population from a list of dictionaries"""    
+
+def calculate_total_population(populations_list):
+  """Calculate total population of all countries from list"""
+  total = 0
+  for population in populations:
+    total = total + int(population['population'])
+    
+  return total
+
+def display_total_population(total):
+  """Convert to million"""
+  total_in_millions = (round(total/1000000))
+  """Print sentence"""
+  print(f"The total population: {total_in_millions} million people")
+
+#Initial data set
+populations = [
+  { 'country': "France", "population": "60000000"},
+  { 'country': "Spain", "population": "50000000"},
+  { 'country': "USA", "population": "30000000"},
+  { 'country': "Australia", "population": "25000000"},
+  { 'country': "Canada", "population": "38000000"},
+]
+
+# Display the total population such as, the total population is 203 million people
+total_population = calculate_total_population(populations)
+display_total_population(total_population)
+
